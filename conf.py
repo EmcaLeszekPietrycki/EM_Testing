75e9967231b11a0aa33a83bd32282816e1ee9301
leszek.pietrycki@it.emca.pl <leszek.pietrycki@it.emca.pl>
2025-05-20 10:09:19 +0200

    5446545664adas6da65asd64dasdas

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 from recommonmark.parser import CommonMarkParser
 
-extensions = ['myst_parser', 'sphinx_tabs.tabs']
+extensions = ['myst_parser', 'sphinx_tabs.tabs', 'sphinx_rtd_size', 'sphinx_rtd_theme']
 templates_path = ['templates', '_templates', '.templates']
 source_suffix = ['.rst', '.md']
 source_parsers = {
@@ -22,6 +22,7 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 htmlhelp_basename = 'energymonitor'
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'body_max_width': None}
 file_insertion_enabled = False
 latex_documents = [
   ('index', 'energymonitor.tex', u'Energy Monitor Documentation',
